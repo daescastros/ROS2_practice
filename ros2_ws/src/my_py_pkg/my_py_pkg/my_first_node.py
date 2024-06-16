5c418e1ff0f7985fa89c7e3fdf796c7187fb5171
Daniel Castro <daescastros@gmail.com>
2024-06-16 16:28:26 -0600

    session1: section 3 lesson 19

--- ros2_ws/src/my_py_pkg/my_py_pkg/my_first_node.py
+++ ros2_ws/src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -2,10 +2,18 @@
 import rclpy
 from rclpy.node import Node
 
+class MyNode(Node):
+    def __init__(self):
+        super().__init__("py_test")
+        self.get_logger().info("Hello ROS2!!")
+        self.create_timer(0.5) # Time in seconds. 0.5s for 2Hz
+
+    def timer_callback(self):
+        self.get_logger().info("Hello")
+
 def main(args=None):
     rclpy.init(args=args)
-    node = Node("py_test") # Create node with name
-    node.get_logger().info("Hello ROS2!")
+    node = MyNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
