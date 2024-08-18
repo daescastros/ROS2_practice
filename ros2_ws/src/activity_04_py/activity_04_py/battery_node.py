2e05be7f3e79a86bdaaa85bc4f2e8412e67470e5
Daniel Castro <daescastros@gmail.com>
2024-08-18 11:44:48 -0600

    Session 13: lesson 72 - Activity 4 - Add time measurement for multiple time related tasks

--- ros2_ws/src/activity_04_py/activity_04_py/battery_node.py
+++ ros2_ws/src/activity_04_py/activity_04_py/battery_node.py
@@ -16,8 +16,51 @@ class BatteryNode(Node):
         self.battery_is_charged_status_ = True
         self.state_ = "off"
         self.led_number_ = 3
-        # Crear dos timer para cuando cambia de estado?
         self.timer_ = self.create_timer(2.0, self.callback_change_led_state_client)
+        """
+        # Segunda opcion de timer:
+        # Medicion del tiempo para crear dos tareas separadas por tiempo:
+        self.battery_state_ = "full"
+        self.last_time_battery_state_changed_ = self.get_current_time_seconds()
+        self.battery_timer_ = self.create_timer(0.1, self.check_battery_state)
+
+    def get_current_time_seconds(self):
+        secs, nanosecs = self.get_clock().now().seconds_nanoseconds()
+        return secs + nanosecs / 1000000000.0
+
+    def check_battery_state(self):
+        time_now = self.get_current_time_seconds()
+        if self.battery_state_ == "full":
+            if time_now - self.last_time_battery_state_changed_ > 4.0:
+                self.battery_state_ = "empty"
+                self.get_logger().info("Battery is empty! Charging battery...")
+                self.last_time_battery_state_changed_ = time_now
+                self.callback_change_led_state_client(self.battery_state_)
+        else:
+            if time_now - self.last_time_battery_state_changed_ > 6.0:
+                self.battery_state_ = "full"
+                self.get_logger().info("Battery is full! Not charging battery...")
+                self.last_time_battery_state_changed_ = time_now
+                self.callback_change_led_state_client(self.battery_state_)
+    
+    def callback_change_led_state_client(self, battery_state):
+        if battery_state == 'full'
+            self.battery_is_charged_status_ = True
+        elif battery_state == 'empty'
+            self.battery_is_charged_status_ = False
+        client = self.create_client(SetLed, "leds_state_service")
+        while not client.wait_for_service(2.0):
+            self.get_logger().warn("Waiting for server LEDs State...")
+        if self.battery_is_charged_status_:
+            self.state_ = "off"
+        else:
+            self.state_ = "on"
+        request = SetLed.Request()
+        request.led_number = self.led_number_
+        request.state = self.state_
+        future = client.call_async(request)
+        future.add_done_callback(self.callback_print_confirm)
+        """
 
     def callback_change_led_state_client(self):
         self.battery_is_charged_status_ = not self.battery_is_charged_status_
