3e807973d2b3eafab6dec3fe73edb7cedd9e21d1
Daniel Castro <daescastros@gmail.com>
2025-03-17 13:12:43 -0600

    created_desk_walking_robot in xacro format This includes use of variables

--- ros2_ws/temp_run_desk_walking_robot.sh
+++ ros2_ws/temp_run_desk_walking_robot.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-ros2 launch urdf_tutorial display.launch.py model:=/home/vm-micros/Documents/ros2_practice/ros2_ws/src/desk_walking_robot_description/urdf/my_robot.urdf
+#ros2 launch urdf_tutorial display.launch.py model:=/home/vm-micros/Documents/ros2_practice/ros2_ws/src/desk_walking_robot_description/urdf/my_robot.urdf
+ros2 launch urdf_tutorial display.launch.py model:=/home/vm-micros/Documents/ros2_practice/ros2_ws/src/desk_walking_robot_description/urdf/my_robot.urdf.xacro
